5808e9dbc432c99f93375de4077c0676f4ee1304
Emrah Turan <mail.emrahturan@gmail.com>
2022-01-13 23:00:12 +0300

    completed 1. task

--- WeeklyDemos/UIComponents/UIComponents/Screens/WebViewContainerViewController.swift
+++ WeeklyDemos/UIComponents/UIComponents/Screens/WebViewContainerViewController.swift
@@ -12,13 +12,18 @@ class WebViewContainerViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-
+    private var showHTML: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        configureWebView()
-        configureActivityIndicator()
+        if showHTML {
+            configureHTMLWebView()
+        } else {
+            configureWebView()
+            configureActivityIndicator()
+        }
     }
 
     var urlString = "https://www.google.com"
@@ -59,16 +64,38 @@ class WebViewContainerViewController: UIViewController {
         }
 
     }
+    
+    func configureHTMLWebView() {
+        webView.loadHTMLString("<html><body><p>Hello!</p></body></html>", baseURL: nil)
+    }
 
     @IBAction func reloadButtonTapped(_ sender: UIBarButtonItem) {
         webView.reload()
     }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        webView.goBack()
+    }
+    @IBAction func forwardButtonTapped(_ sender: Any) {
+        webView.goForward()
+    }
+    
+    @IBAction func safariButtonTapped(_ sender: Any) {
+        guard let url = URL(string: webView.url?.absoluteString ?? "") else { return }
+        UIApplication.shared.open(url)
+    }
 }
 
 extension WebViewContainerViewController: WKNavigationDelegate {
-
+    //Change the font and size
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            let changeFontFamilyScript = "document.getElementsByTagName(\'body\')[0].style.fontFamily = \"Impact,Charcoal,sans-serif\";" +
+                                         "document.getElementsByTagName('body')[0].style.webkitTextSizeAdjust= '200%'"
+            webView.evaluateJavaScript(changeFontFamilyScript) { (response, error) in
+                
+            }
+        }
 }
 
 extension WebViewContainerViewController: WKUIDelegate {
-
 }
